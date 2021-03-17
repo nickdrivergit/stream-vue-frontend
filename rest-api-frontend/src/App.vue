3daf7348b8242e3ddaf16cabbdb7b87af0eaca01
nick <nicholasdriver@protonmail.com>
2021-03-17 14:12:40 +0200

    initial commit

---
+++ rest-api-frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+      arrClassroom: []
+    }
+  },
+  async created() {
+    try{
+      const { data } = await axios.get("http://localhost:1234/get-students");
+      // data.forEach(student => {
+        // console.log(this.arrClassroom);
+        // this.arrClassroom[student.classroom]++;
+      // });
+      console.log(data);
+    } catch(err){
+      console.error(err);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
